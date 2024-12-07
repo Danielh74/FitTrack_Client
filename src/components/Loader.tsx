3af721da500ce847c548a80ac3bd384e78873f87
Danielh74 <danielh07042000@gmail.com>
2024-12-07 20:18:16 +0200

    updated loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,14 +1,17 @@
 import { MutatingDots } from 'react-loader-spinner'
+import useTheme from '../hooks/useTheme';
 
 
 const Loader = () => {
+    const { darkMode } = useTheme();
+
     return (
         <MutatingDots
             visible={true}
             height="100"
             width="100"
-            color="#2563EB"
-            secondaryColor="#2563EB"
+            color={darkMode ? "#FFD700" : "#274C77"}
+            secondaryColor={darkMode ? "#FFD700" : "#274C77"}
             radius="12.5"
             ariaLabel="mutating-dots-loading"
             wrapperStyle={{}}
